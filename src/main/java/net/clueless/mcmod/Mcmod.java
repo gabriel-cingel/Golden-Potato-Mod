0ea112039df647a9b33469d7f0fef7acbce360d9
Clueless <gabriel.cingel@gmail.com>
2025-05-26 12:04:17 +0100

    comment added

--- src/main/java/net/clueless/mcmod/Mcmod.java
+++ src/main/java/net/clueless/mcmod/Mcmod.java
@@ -8,7 +8,7 @@ import org.slf4j.LoggerFactory;
 public class Mcmod implements ModInitializer {
 	public static final String MOD_ID = "mcmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
+// comment
 	@Override
 	public void onInitialize() {
 
